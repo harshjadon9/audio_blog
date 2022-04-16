06b13a31b1b723808fd78342db27c4c31f2904e2
harshjadon9 <62889318+harshjadon9@users.noreply.github.com>
2022-04-16 14:57:48 +0530

    Update pricing.js

--- src/crc/components/pricing.js
+++ src/crc/components/pricing.js
@@ -4,8 +4,10 @@ const Price = ()=>{
     return(
         <section className="bg-white h-full text-black py-10">
         <h1 className="font-bold">Simple <span className="mx-1 font-extrabold underline decoration-8 decoration-[#c9c9c9]">Pricing</span> Plans</h1>
-        <div>
-            <></>
+        <div className="flex">
+            <div>
+
+            </div>
         </div>
         </section>
     )
