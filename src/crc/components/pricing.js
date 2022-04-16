263b74ef824827d91f7f242d31794ac3f1ba12c0
harshjadon9 <62889318+harshjadon9@users.noreply.github.com>
2022-04-16 14:51:28 +0530

    Update pricing.js

--- src/crc/components/pricing.js
+++ src/crc/components/pricing.js
@@ -3,7 +3,10 @@ import React from "react";
 const Price = ()=>{
     return(
         <section className="bg-white h-full text-black py-10">
-        <h1>Pricing</h1>
+        <h1 className="font-bold">Simple <span className="mx-1 font-extrabold underline decoration-8 decoration-[#c9c9c9]">Pricing</span> Plans</h1>
+        <div>
+            <></>
+        </div>
         </section>
     )
 }
